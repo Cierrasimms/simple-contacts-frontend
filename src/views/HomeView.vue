26bc027ddc0a7e54c410562cd49b61c90ea02053
CierraSimms <cierrasimms98@gmail.com>
2022-04-06 19:56:17 -0400

    added showContacts method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for="contact in contacts" v-bind:key="contact.id">
-      <h2>{{ contact.first_name }}</h2>
-      <h2>{{ contact.last_name }}</h2>
-      <h2>{{ contact.email }}</h2>
-      <h2>{{ contact.phone_number }}</h2>
-    </div>
+    <div v-for="contact in contacts" v-bind:key="contact.id"></div>
     <div>
       <h1>New Contact</h1>
       First name:
@@ -19,6 +14,26 @@
       <input type="text" v-model="newContact.phone_number" />
       <button v-on:click="createContacts()">Create New Contact</button>
     </div>
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+      <h2>{{ contact.first_name }}</h2>
+      <h2>{{ contact.last_name }}</h2>
+
+      <!-- <p>First name: {{ contact.first_name }}</p>
+      <p>Last name: {{ contact.last }}</p>
+      <p>Email: {{ contact.email }}</p>
+      <p>Phone number: {{ contact.phone_number }}</p> -->
+      <button v-on:click="showContacts(contact)">More Info</button>
+    </div>
+    <dialog id="contact-details">
+      <form method="dialog">
+        <h1>Contact Info</h1>
+        <p>First name: {{ currentContact.first_name }}</p>
+        <p>Last name: {{ currentContact.last_name }}</p>
+        <p>Email: {{ currentContact.email }}</p>
+        <p>Phone number: {{ currentContact.phone_number }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -31,6 +46,7 @@ export default {
       message: "Contacts",
       contacts: [],
       newContact: {},
+      currentContact: {},
     };
   },
   created: function () {
@@ -50,6 +66,10 @@ export default {
         this.contacts.push(response.data);
         this.newContacts = {};
       });
+    },
+    showContacts(contact) {
+      this.currentContact = contact;
+      document.querySelector("#contact-details").showModal();
     },
   },
 };
